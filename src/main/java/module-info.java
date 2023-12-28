c494cc79bfac343e890eed74fd43dafce5c1e185
um-saltanat <145360854+um-saltanat@users.noreply.github.com>
2023-12-28 08:41:48 +0600

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.recipe_dbms {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.recipe_dbms to javafx.fxml;
-    exports com.example.recipe_dbms;
-}
